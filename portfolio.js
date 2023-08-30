ef2771192456040f86543fdbafdbd75eb351d7c1
Andani Rananga <andani.rananga@younglings.africa>
2023-08-30 14:14:37 +0200

    i was working on the responsive of the home and about page

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,10 @@
 
-
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('header a').forEach(link => {
+  if(link.href.includes(`${activePage}`)){
+    link.classList.add('active');
+    console.log(link);
+  }
+})
 
 
