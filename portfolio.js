4a493af62341c607194ac831c25469a515acbf87
Andani Rananga <andani.rananga@younglings.africa>
2023-09-04 16:10:15 +0200

    form validation for name, email and phone numbes

--- portfolio.js
+++ portfolio.js
@@ -7,3 +7,90 @@ menu.onclick = () => {
   navbar.classList.toggle("active");
 };
 
+  var nameError = document.getElementById("name-error");
+  var emailError = document.getElementById("email-error");
+  var phoneError = document.getElementById("phone-error");
+  var messageError = document.getElementById("message-error");
+  var submitError = document.getElementById("submit-error");
+
+  function validateName(){
+
+    var name=document.getElementById("contact-name").value;
+    if(name.length==0)
+    {
+      nameError.innerHTML = "Required";
+      return false;
+    }
+    if(!name.match(/^[A-Za-z]+ [A-Za-z]+$/)){
+
+      nameError.innerHTML = "write full name";
+      return false;
+    }
+
+    nameError.innerHTML = '<i class="fa-sharp fa-solid fa-circle-check"></i>';
+    return true;
+     
+  }
+
+  function validatePhone(){
+
+    var phone = document.getElementById("contact-phone").value;
+    if(phone.length==0){
+      phoneError.innerHTML = "required"
+      return false;
+    }
+    if(phone.length!==10){
+      phoneError.innerHTML = "10 digits required";
+      return false;
+    }
+    if(!phone.match(/^[0-9]{10}$/)){
+
+      phoneError.innerHTML = "only numbers please";
+      return false;
+    }
+
+    phoneError.innerHTML = '<i class="fa-sharp fa-solid fa-circle-check"></i>';
+    return true;
+
+
+  }
+
+  function validateEmail(){
+
+    var email = document.getElementById("contact-email").value;
+
+
+    if(email.length==0){
+      emailError.innerHTML = "Email is required";
+      return false;
+    }
+    if(!email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)){
+
+      emailError.innerHTML = "Email Invalid";
+      return false;
+    }
+    emailError.innerHTML = '<i class="fa-sharp fa-solid fa-circle-check"></i>';
+    return true;
+  }
+
+  
+  function validateMessage(){
+var message = document.getElementById("contact-message").value;
+var words= 30;
+var left = words - message.length;
+
+if(left > 0){
+
+  messageError.innerHTML = left + "more words required";
+  return false;
+}
+messageError.innerHTML = '<i class="fa-sharp fa-solid fa-circle-check"></i>';
+return true;
+  }
+
+  function validateForrm(){
+    if(!validateName() || !validatePhone() || !validateEmail() || !validateMessage()){
+      submitError.innerHTML = "Please fix errors";
+      return false;
+    }
+  }
